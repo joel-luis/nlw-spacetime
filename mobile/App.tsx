ac10e9de8b176771d1b62d78b0e263250c649c6d
Joel Luis <joelluis73@gmail.com>
2023-05-20 15:25:21 -0300

    create layout home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,55 @@
+import { styled } from 'nativewind'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+
+import { useFonts } from 'expo-font'
+import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NlwLogo from './src/assets/nlw-spacetime-logo.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-2xl font-bold text-zinc-50">
-        Open up App.js to start working on your app!
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NlwLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
+          <Text className="tex-sm font-alt uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
