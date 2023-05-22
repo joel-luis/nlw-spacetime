9b1f4793b351cce45c03586b7b14e333ddaa7a2c
Joel Luis <joelluis73@gmail.com>
2023-05-22 20:06:24 -0300

    finised layout home mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8"
+      className="relative flex-1 items-center border bg-gray-900 px-8 py-10"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
@@ -42,12 +42,19 @@ export default function App() {
             quiser) com o mundo!
           </Text>
         </View>
-        <TouchableOpacity className="rounded-full bg-green-500 px-5 py-3">
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+        >
           <Text className="tex-sm font-alt uppercase text-black">
             Cadastrar lembrança
           </Text>
         </TouchableOpacity>
       </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
